68f8082b7f61a0d687900e642896650c275a0862
Shawn Wilson <swilson@bluelyte.com>
2019-09-28 18:00:32 -0400

    change build

--- upload_alpha_to_s3.sh
+++ upload_alpha_to_s3.sh
@@ -4,4 +4,4 @@ export BUILD_DATE=$(date +%Y%m%d%H%M)
 
 aws s3 cp build/libs/*.jar s3://${BUCKET_NAME}/alpha/morecreeps-alpha-${BUILD_DATE}.jar
 
-aws lambda invoke --function-name morecreeps-post-alpha-build --payload "{\"buildDate\": \"${BUILD_DATE}\"}" /dev/stdout
+aws lambda invoke --function-name morecreeps-discord-post-alpha-build --payload "{\"buildDate\": \"${BUILD_DATE}\"}" /dev/stdout
